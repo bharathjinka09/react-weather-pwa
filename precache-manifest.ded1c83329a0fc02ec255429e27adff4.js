e411878fe379301681b71865471f73da1abf6b97
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-28 17:04:27 +0530

    Updates

---
+++ precache-manifest.ded1c83329a0fc02ec255429e27adff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae01fc8668b84357b52c31435d359d8a",
+    "url": "/react-weather-pwa/index.html"
+  },
+  {
+    "revision": "047854535d32c411fec9",
+    "url": "/react-weather-pwa/static/css/main.ac503996.chunk.css"
+  },
+  {
+    "revision": "3696c363641e190a7541",
+    "url": "/react-weather-pwa/static/js/2.d54fc02e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-pwa/static/js/2.d54fc02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "047854535d32c411fec9",
+    "url": "/react-weather-pwa/static/js/main.29091a98.chunk.js"
+  },
+  {
+    "revision": "d9ce1dcdb5f98346e658",
+    "url": "/react-weather-pwa/static/js/runtime-main.6f4582ba.js"
+  }
+]);
